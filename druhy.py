e0d1047755c5e11bbfcc8c31cbd7fd310fae89f6
Marcel Novotny <marceln@xcroco.com>
2019-12-09 14:32:00 +0100

    initial release

--- druhy.py
+++ druhy.py
@@ -1,10 +1,45 @@
-while True:
-    x=int(input("zadej cislo"))
-    if x>0 :
-        print ("Kladne")
-    elif x<0 :
-        print ("zaporne")
-    elif x==0 :
-        print ("nula")
+# while True:
+#     x=int(input("zadej cislo"))
+#     if x>0 :
+#         print ("Kladne")
+#     elif x<0 :
+#         print ("zaporne")
+#     elif x==0 :
+#         print ("nula")
+
+# a=[1,2,3,4,5,0,-3]
+# b=[1,2,3,4,5]
+# #
 
 
+#faktorial
+
+# a=1
+# n=10
+# for i in range(1,n):
+#     a*=i
+#     print(a)
+import itertools, time
+
+class FactorialSeq:
+
+    def __init__(self):
+        self.a=1
+        self.i=1
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        self.a*=self.i
+        self.i+=1
+        return(self.a)
+
+
+# fact_seq()
+
+
+seq = FactorialSeq()
+for item in seq:
+    print(item)
+    time.sleep(1)
